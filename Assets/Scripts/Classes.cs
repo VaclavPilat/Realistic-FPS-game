e53fadbabc3568b27f58b8c8530f1bc311bac7a1
Václav Pilát <pilatvacl@seznam.cz>
2022-02-27 21:10:05 +0100

    Added a sound manager script

--- Assets/Scripts/Classes.cs
+++ Assets/Scripts/Classes.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using System;
 
 // Keybind
-[Serializable]
 public class Keybind
 {
     public string Name; // Name of the keybind
@@ -20,4 +20,16 @@ public class Map_Info
     public string Description; // Map decription
     public string Size; // Map size
     public Game_Mode[] Modes; // Game modes compatible with this map
+}
+
+// Sound
+[Serializable]
+public class Sound
+{
+    public string Name; // Sound name
+    public AudioClip Clip; // Audio clip
+    public float Volume = 1f; // Default volume
+    public bool Loop = false; // Enable looping?
+    public float Spatial = 1f; // 0=2D; 1=3D
+    [HideInInspector] public AudioSource Source; // Audio source component
 }
---
+++ Assets/Scripts/Sound manager/Sound_Manager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+// https://www.youtube.com/watch?v=6OT43pvUyfY
+// Sound manager
+public class Sound_Manager : MonoBehaviour
+{
+    //##########################################################################################
+    //#############################  PRIVATE METHODS / VARIABLES  ##############################
+    //##########################################################################################
+
+    [SerializeField] private Sound[] Sounds; // List of sounds
+
+
+    //##########################################################################################
+    //###################################### SCRIPT FLOW #######################################
+    //##########################################################################################
+
+    // Applying sound properties to newly created AudioSource components
+    private void Awake ()
+    {
+        foreach(Sound s in Sounds)
+        {
+            s.Source = transform.gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.Clip;
+            s.Source.volume = s.Volume;
+            s.Source.loop = s.Loop;
+            s.Source.spatialBlend = s.Spatial;
+        }
+    }
+
+    // Finding sound by name
+    private Sound Get_Sound (string name)
+    {
+        Sound s = Array.Find(Sounds, sound => sound.Name == name);
+		if (s == null)
+			Console.Warning(this, "Sound '" + name + "' not found!");
+        return s;
+    }
+
+    // Play sound
+    public void Play (string name)
+    {
+        Sound s = Get_Sound(name);
+        if(s != null)
+            s.Source.Play();
+    }
+
+}
