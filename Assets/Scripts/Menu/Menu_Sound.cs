7d88ca05f33f2cc4c5626c1e7eec1ca1eb248755
Václav Pilát <pilatvacl@seznam.cz>
2022-03-01 11:24:34 +0100

    Enabled onclick sound for InputField

--- Assets/Scripts/Menu/Menu_Sound.cs
+++ Assets/Scripts/Menu/Menu_Sound.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-// Getting basic element properties
-public class Menu_Button : MonoBehaviour
+// Binding sounds to input elements
+public class Menu_Sound : MonoBehaviour
 {
     //##########################################################################################
     //######################################  COMPONENTS  ######################################
     //##########################################################################################
 
     private Button Button = null; // Button component
+    private InputField InputField = null; // InputField component
     private Sound_Manager Sound_Manager; // Sound manager
+
+
+    //##########################################################################################
+    //#############################  PRIVATE METHODS / VARIABLES  ##############################
+    //##########################################################################################
+
+    private bool InputField_Enabled = false; // Enabling sounds for input fields 
 
 
     //##########################################################################################
@@ -24,11 +32,28 @@ public class Menu_Button : MonoBehaviour
     //#####################################  SCRIPT FLOW  ######################################
     //##########################################################################################
 
-    public void Awake ()
+    // Enabling sounds
+    private void Awake ()
     {
-        Button = GetComponent<Button>(); // Getting Button component
+        // Setting up sound manager
         Sound_Manager = transform.root.GetComponent<Sound_Manager>();
-        Button.onClick.AddListener(Onclick_Sound);
+        // Getting Button component
+        Button = GetComponent<Button>(); 
+        if(Button != null)
+            Button.onClick.AddListener(Onclick_Sound);
+        // Getting InputField component
+        InputField = GetComponent<InputField>();
+    }
+
+    // Enabling sounds for InputField
+    private void Update ()
+    {
+        if(InputField != null)
+        {
+            if(!InputField_Enabled && InputField.isFocused)
+                Onclick_Sound();
+            InputField_Enabled = InputField.isFocused;
+        }
     }
 
     // Making sound on click
