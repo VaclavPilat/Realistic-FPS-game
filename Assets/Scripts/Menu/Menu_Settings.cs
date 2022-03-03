c61a2f092db99b7a4684df8ae39981a784c4e038
Václav Pilát <pilatvacl@seznam.cz>
2022-03-03 13:12:37 +0100

    Saving slider settings after edit

--- Assets/Scripts/Menu/Menu_Settings.cs
+++ Assets/Scripts/Menu/Menu_Settings.cs
@@ -49,16 +49,7 @@ public class Menu_Settings : MonoBehaviour
                 switch(setting.Type)
                 {
                     case "float": // Showing sliders for float values
-                        // Setting up label
-                        GameObject label = Instantiate(Description, row.transform);
-                        label.GetComponent<Text>().text = setting.Description;
-                        // Setting up slider
-                        GameObject input = Instantiate(Slider, row.transform);
-                        var slider = input.GetComponent<Slider>();
-                        string[] check = setting.Check.Split('|');
-                        slider.minValue = float.Parse(check[0]);
-                        slider.maxValue = float.Parse(check[1]);
-                        slider.value = float.Parse(setting.Value);
+                        Generate_Slider(setting, row);
                         break;
                     default:
                         break;
@@ -73,6 +64,30 @@ public class Menu_Settings : MonoBehaviour
         }
         // Resizing scrollview
         transform.GetComponent<Menu_Scrollview>().Resize();
+    }
+
+    // Generating slider with a description label
+    private void Generate_Slider (Setting setting, GameObject row)
+    {
+        // Setting up label
+        GameObject label = Instantiate(Description, row.transform);
+        label.GetComponent<Text>().text = setting.Description;
+        // Setting up slider
+        GameObject input = Instantiate(Slider, row.transform);
+        var slider = input.GetComponent<Slider>();
+        string[] check = setting.Check.Split('|');
+        slider.minValue = float.Parse(check[0]);
+        slider.maxValue = float.Parse(check[1]);
+        slider.value = float.Parse(setting.Value);
+        slider.onValueChanged.AddListener(delegate { Save_Slider(slider); });
+    }
+
+    // Saving slider value after being changed
+    private void Save_Slider (Slider slider)
+    {
+        int index = slider.transform.parent.GetSiblingIndex();
+        Config_Loader.Config[Name][index].Value = slider.value.ToString();
+        Config_Loader.Save(Name);
     }
 
 }
