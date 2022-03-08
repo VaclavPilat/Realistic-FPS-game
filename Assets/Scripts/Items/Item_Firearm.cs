9c723f00d43017cd2ed0b9b477d5d51b5779d8ca
Václav Pilát <pilatvacl@seznam.cz>
2022-03-08 23:09:11 +0100

    Implemented chances for gun and ammo failure

--- Assets/Scripts/Items/Item_Firearm.cs
+++ Assets/Scripts/Items/Item_Firearm.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Random = UnityEngine.Random;
 
 // Guns
 public class Item_Firearm : Item
@@ -24,7 +25,9 @@ public class Item_Firearm : Item
     //#############################  PRIVATE METHODS / VARIABLES  ##############################
     //##########################################################################################
 
-    private bool Bullet_In_Chamber = false; // Bullet in chamber?
+    private bool Ammo_In_Chamber = false; // Bullet in chamber?
+    private bool Casing_In_Chamber = false; // Is an empty casing still in chanber?
+
     private bool Ammo_Failed = false; // Did the ammunition fail to shoot?
     private bool Casing_Stuck = false; // Is the bullet case stuck in the chamber, making the gun unable to shoot?
 
@@ -48,15 +51,11 @@ public class Item_Firearm : Item
     // Shooting
     public override bool Primary ()
     {
-        if(Bullet_In_Chamber)
-        {
-            Shoot();
-            if(Mode == Firearm_Mode.Semi || Mode == Firearm_Mode.Auto)
+        bool value = Shoot();
+        if(Mode == Firearm_Mode.Semi || Mode == Firearm_Mode.Auto)
+            if(!Ammo_Failed)
                 Reload();
-            return true;
-        }
-        else
-            return false;
+        return value;
     }
 
     // Aiming down sights
@@ -71,10 +70,42 @@ public class Item_Firearm : Item
         return true;
     }
 
-    // 
+    // Reloading the weapon - getting new ammo into the chamber
     public override bool Reload () 
     {
-        Bullet_In_Chamber = true;
+        // Fixing issue with stuck ammo casing
+        if(Casing_Stuck)
+        {
+            if(Random.Range(0f, 1f) <= 0.8f)
+            {
+                Console.Log(this, "Ammo casing unstuck");
+                Casing_Stuck = false;
+                Casing_In_Chamber = false;
+                Ammo_In_Chamber = false;
+            }
+            else
+            {
+                Console.Log(this, "Ammo casing still stuck");
+                return false;
+            }
+        }
+        // Getting ammo casing out and adding new ammo to chamber
+        if(!Casing_Stuck)
+        {
+            if((Casing_In_Chamber || Ammo_In_Chamber) && Random.Range(0f, 1f) <= Gun_Failure) // Ammo casing stuck
+            {
+                Console.Log(this, "Ammo casing stuck");
+                Casing_Stuck = true;
+                return false;
+            }
+            else // Old casing out, new ammo in
+            {
+                Console.Log(this, "New ammo loaded");
+                Casing_In_Chamber = false;
+                Ammo_In_Chamber = true;
+                Ammo_Failed = false;
+            }
+        }
         return true;
     }
 
@@ -85,14 +116,45 @@ public class Item_Firearm : Item
     }
 
     // Shooting
-    private void Shoot ()
+    private bool Shoot ()
     {
-        Sound_Manager.Play("Fire");
-        GameObject bullet = Instantiate(Bullet, Bullet_Spawn);
-        bullet.transform.SetParent(null);
-        bullet.GetComponent<Rigidbody>().velocity = Bullet_Spawn.TransformDirection(new Vector3(-Bullet_Speed, 0f, 0f));
-        //bullet.GetComponent<Projectile_Bullet>().Damage = Damage;
-        Bullet_In_Chamber = false;
+        // Ammo casing is stuck
+        if(Casing_Stuck)
+        {
+            Console.Log(this, "Ammo is stuck, cannot fire");
+            return false;
+        }
+        // Ammo failed to fire
+        if(Ammo_Failed)
+        {
+            Console.Log(this, "Ammo already failed");
+            return false;
+        }
+        // Ammo not in chamber
+        if(!Ammo_In_Chamber)
+        {
+            Console.Log(this, "No ammo in chamber");
+            return false;
+        }
+        // Ammo failure chance
+        if(Random.Range(0f, 1f) <= Ammo_Failure)
+        {
+            Console.Log(this, "Ammo failed to fire");
+            Ammo_Failed = true;
+            return false;
+        }
+        else
+        {
+            Console.Log(this, "Ammo fired");
+            Sound_Manager.Play("Fire");
+            GameObject bullet = Instantiate(Bullet, Bullet_Spawn);
+            bullet.transform.SetParent(null);
+            bullet.GetComponent<Rigidbody>().velocity = Bullet_Spawn.TransformDirection(new Vector3(-Bullet_Speed, 0f, 0f));
+            //bullet.GetComponent<Projectile_Bullet>().Damage = Damage;
+            Ammo_In_Chamber = false;
+            Casing_In_Chamber = true;
+            return true;
+        }
     }
 
 
@@ -104,9 +166,9 @@ public class Item_Firearm : Item
     {
         string output = "";
         output += "Bleeding caused by item: " + Bleeding.ToString() + " ml/s\n";
-        output += "Gun failure chance: " + Math.Round(Gun_Failure*100, 2).ToString() + "%\n";
-        output += "Ammo failure chance: " + Math.Round(Ammo_Failure*100, 2).ToString() + "%\n";
-        output += "Bullet in chamber: " + Bullet_In_Chamber.ToString() + "\n";
+        output += "Gun failure chance: " + Math.Round(Gun_Failure*100, 2).ToString() + "%" + (Casing_Stuck ? " - CASING STUCK" : null) + "\n";
+        output += "Ammo failure chance: " + Math.Round(Ammo_Failure*100, 2).ToString() + "%" + (Ammo_Failed ? " - AMMO FAILED" : null) + "\n";
+        output += "Bullet in chamber: " + Ammo_In_Chamber.ToString() + "\n";
         return output;
     }
 
--- Assets/Scripts/Projectiles/Projectile_Bullet.cs
+++ Assets/Scripts/Projectiles/Projectile_Bullet.cs
@@ -19,14 +19,14 @@ public class Projectile_Bullet : MonoBehaviour
     // Move after being spawned
     private void Start () 
     {
-        Console.Log(this, "SPAWN");
+        //Console.Log(this, "SPAWN");
         StartCoroutine(Despawn_After_Time(3f));
     }
 
     // Despawning bullet after hit
     private void OnCollisionEnter (Collision collision)
     {
-        Console.Log(this, "HIT " + collision.transform.name);
+        //Console.Log(this, "HIT " + collision.transform.name);
         GameObject.Destroy(gameObject);
     }
 
@@ -34,7 +34,7 @@ public class Projectile_Bullet : MonoBehaviour
     private IEnumerator Despawn_After_Time (float seconds)
     {
         yield return new WaitForSeconds(seconds);
-        Console.Log(this, "DESPAWNED");
+        //Console.Log(this, "DESPAWNED");
         GameObject.Destroy(gameObject);
     }
 
