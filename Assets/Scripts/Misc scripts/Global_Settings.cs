3d879434500d877520d958940f8a10e63e3398c9
Václav Pilát <pilatvacl@seznam.cz>
2022-03-17 20:26:35 +0100

    Setting graphics quality preset

---
+++ Assets/Scripts/Misc scripts/Global_Settings.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// Script for updating global settings once in a while
+public class Global_Settings : MonoBehaviour
+{
+    //##########################################################################################
+    //#############################  PRIVATE METHODS / VARIABLES  ##############################
+    //##########################################################################################
+
+    private int Last_Quality = -1; // Last value of graphics quality
+
+
+    //##########################################################################################
+    //#####################################  SCRIPT FLOW  ######################################
+    //##########################################################################################
+    
+    // Loading necessary settings
+    private void Start()
+    {
+        Config_Loader.Load("Graphics");
+        // Starting a coroutine that periodically applies the global settings
+        StartCoroutine(Repeat(2f));
+    }
+
+    // Applying global settings
+    private IEnumerator Repeat (float period)
+    {
+        while(true)
+        {
+            // Waiting for a specified amount of time
+            yield return new WaitForSecondsRealtime(period);
+            // Setting graphics quality
+            Setting setting = Array.Find(Config_Loader.Config["Graphics"], s => s.Name == "Quality");
+            if(setting != null)
+            {
+                int index = int.Parse(setting.Value);
+                if(Last_Quality != index)
+                {
+                    Console.Log(this, "Setting new graphics quality to " + index.ToString() + " = " + setting.Check.Split('|')[index]);
+                    QualitySettings.SetQualityLevel(index, true);
+                    Last_Quality = index;
+                }
+            }
+            else
+                Console.Warning(this, "Graphics quality setting cannot be found");
+        }
+    }
+
+}
