bc0d73704dedaa8ad29af72a9d2ee98ea68c1535
Václav Pilát <pilatvacl@seznam.cz>
2022-04-18 11:12:23 +0200

    Starting timer after player triggers the start trigger

--- Assets/Scripts/Maps/Target practice/Target_Counter.cs
+++ Assets/Scripts/Maps/Target practice/Target_Counter.cs
@@ -22,7 +22,8 @@ public class Target_Counter : MonoBehaviour
     private int Destroyed = -1; // Number of destroyed paper targets
     private float Time_Since = -1f; // Time since the start of the app
     private float Current_Time = -1f; // Time spent on this map
-
+    private bool Started = false; // Is the run started?
+    private bool Ended = false; // Is the run ended?
 
 
     //##########################################################################################
@@ -46,14 +47,37 @@ public class Target_Counter : MonoBehaviour
             if(Total < 0)
                 Total = paper_targets.Count;
             Destroyed = Total - paper_targets.Count;
-            if(Destroyed != 0 && Time_Since < 0)
+            if(Started && Time_Since < 0)
                 Time_Since = Time.time;
-            if(paper_targets.Count != 0 && Time_Since >= 0)
+            if(!Ended && paper_targets.Count != 0 && Time_Since >= 0)
                 Current_Time = Time.time - Time_Since;
+            if(!Ended && Destroyed == Total)
+                End_Run();
             // Showing variables
             Count.text = Destroyed.ToString() + "/" + Total.ToString();
             Timer.text = (Current_Time < 0 ? "0.00" : Current_Time.ToString("0.00")) + "s";
             yield return new WaitForSeconds(0.1f);
         }
+    }
+
+    // Starting the current run
+    private void Start_Run ()
+    {
+        Started = true;
+        Console.Log(this, "Run started");
+    }
+
+    // Ending the current run
+    private void End_Run ()
+    {
+        Ended = true;
+        Console.Log(this, "Run ended");
+    }
+
+    // Starting run after the player triggers the start collider
+    private void OnTriggerEnter (Collider collider)
+    {
+        if(!Started && (collider.gameObject.layer == 7 || collider.gameObject.layer == 8))
+            Start_Run();
     }
 }
