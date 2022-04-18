ae8a34f6f3007a6e124dc447630230116e6ea7fd
Václav Pilát <pilatvacl@seznam.cz>
2022-04-18 10:46:39 +0200

    Tracking number of remaining paper targets

--- Assets/Scripts/Maps/Target practice/Target_Counter.cs
+++ Assets/Scripts/Maps/Target practice/Target_Counter.cs
@@ -2,22 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class Target_Counter : MonoBehaviour
 {
+    //##########################################################################################
+    //######################################  COMPONENTS  ######################################
+    //##########################################################################################
+
+    [SerializeField] private Text Count;
+    [SerializeField] private Text Timer;
+
+
     //##########################################################################################
     //#############################  PRIVATE METHODS / VARIABLES  ##############################
     //##########################################################################################
 
     private int Total = -1; // Total number of paper targets
     private int Destroyed = -1; // Number of destroyed paper targets
+    private float Time_Since = -1f; // Time since the start of the app
+    private float Current_Time = -1f; // Time spent on this map
+
 
 
     //##########################################################################################
     //#####################################  SCRIPT FLOW  ######################################
     //##########################################################################################
 
-    private void Start () => StartCoroutine(Check_Targets());
+    private void Awake () => StartCoroutine(Check_Targets());
 
     // Checking target count
     private IEnumerator Check_Targets()
@@ -34,9 +46,14 @@ public class Target_Counter : MonoBehaviour
             if(Total < 0)
                 Total = paper_targets.Count;
             Destroyed = Total - paper_targets.Count;
+            if(Destroyed != 0 && Time_Since < 0)
+                Time_Since = Time.time;
+            if(paper_targets.Count != 0 && Time_Since >= 0)
+                Current_Time = Time.time - Time_Since;
             // Showing variables
-            Console.Warning(this, Destroyed.ToString() + "/" + Total.ToString());
-            yield return new WaitForSeconds(0.25f);
+            Count.text = Destroyed.ToString() + "/" + Total.ToString();
+            Timer.text = (Current_Time < 0 ? "0.00" : Current_Time.ToString("0.00")) + "s";
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
