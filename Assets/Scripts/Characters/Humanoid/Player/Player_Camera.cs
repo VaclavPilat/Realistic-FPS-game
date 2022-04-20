9e85752e1ab00517c6d871b89c4ea1e587611857
Václav Pilát <pilatvacl@seznam.cz>
2022-04-20 10:58:00 +0200

    Added a setting for mouse sensitivity

--- Assets/Scripts/Characters/Humanoid/Player/Player_Camera.cs
+++ Assets/Scripts/Characters/Humanoid/Player/Player_Camera.cs
@@ -22,7 +22,6 @@ public class Player_Camera : Lockable_Script
     //##################################  PUBLIC VARIABLES  ####################################
     //##########################################################################################
 
-    public float Sensitivity = 100f; // Mouse Sensitivity
     public float Vertical = 90f; // Vertical rotation limit
     public float Horizontal = -1f; // Horizontal rotation limit
 
@@ -48,18 +47,18 @@ public class Player_Camera : Lockable_Script
     {
         if(Horizontal >= 0) // Using different rotation method when horizontal limit is used
         {
-            horizontal_rotation += Input.GetAxis("Mouse X") * Sensitivity * Time.fixedDeltaTime;
+            horizontal_rotation += Input.GetAxis("Mouse X") * float.Parse(Config_Loader.Get("Mouse", "Sensitivity").Value) * Time.fixedDeltaTime;
             horizontal_rotation = Mathf.Clamp(horizontal_rotation, -Horizontal, Horizontal); // Clamping rotation so the player will not be able to look behind
             transform.Rotate(0.0f, horizontal_rotation, 0.0f); // Applying horizontal rotation to the player
         }
         else
-            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Sensitivity * Time.fixedDeltaTime);
+            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * float.Parse(Config_Loader.Get("Mouse", "Sensitivity").Value) * Time.fixedDeltaTime);
     }
 
     // ROTATING Human_Camera.Camera
     private void Rotate_Camera ()
     {
-        vertical_rotation -= Input.GetAxis("Mouse Y") * Sensitivity * Time.fixedDeltaTime;
+        vertical_rotation -= Input.GetAxis("Mouse Y") * float.Parse(Config_Loader.Get("Mouse", "Sensitivity").Value) * Time.fixedDeltaTime;
         vertical_rotation = Mathf.Clamp(vertical_rotation, -Vertical, Vertical); // Clamping rotation so the player will not be able to look behind
         Human_Camera.Camera.transform.localRotation = Quaternion.Euler(vertical_rotation, 0.0f, 0.0f); // Applying vertical rotation to the Camera
     }
@@ -72,7 +71,7 @@ public class Player_Camera : Lockable_Script
     public override string ToString ()
     {
         string output = "";
-        output += "Sensitivity: " + Sensitivity.ToString() + "\n";
+        output += "Sensitivity: " + float.Parse(Config_Loader.Get("Mouse", "Sensitivity").Value).ToString() + "\n";
         output += "Rotation limit (±): ↕ " + Vertical.ToString() + "°; ↔ " + ((Horizontal < 0) ? "none" : Horizontal.ToString() + "°") + "\n";
         output += "Player rotation: " + transform.rotation.eulerAngles.ToString() + " deg\n";
         output += "Camera rotation: " + Human_Camera.Camera.transform.localRotation.eulerAngles.ToString() + " deg\n";
