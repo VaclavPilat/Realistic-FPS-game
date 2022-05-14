8265b7f496a637be6bd80a84acd692fc19453c96
Václav Pilát <pilatvacl@seznam.cz>
2022-05-14 23:34:02 +0200

    Removed unused properties from config files

--- Assets/Scripts/Classes.cs
+++ Assets/Scripts/Classes.cs
@@ -52,11 +52,11 @@ public class Sound
 [Serializable]
 public class Setting
 {
-    public string Name; // Setting name, used internally
-    public string Description; // Setting description, shown to user
-    public string Type; // Setting type
-    public string Check; // Setting rules for checking validity
-    public string Value; // Setting value, stored as an object
+    public string Name = ""; // Setting name, used internally
+    public string Description = ""; // Setting description, shown to user
+    public string Type = ""; // Setting type
+    public string Check = ""; // Setting rules for checking validity
+    public string Value = ""; // Setting value, stored as an object
 
     public Keybind ToKeybind ()
     {
