04393582414851de25443887496a5f19ce22cefb
Václav Pilát <pilatvac@fit.cvut.cz>
2023-01-17 15:46:47 +0100

    Recognizing and replacing pattern of a tower

--- Assets/Scripts/Maps/Fortress/Fortress_Generator.cs
+++ Assets/Scripts/Maps/Fortress/Fortress_Generator.cs
@@ -2,19 +2,6 @@ using System.Collections;
 using UnityEngine;
 using System;
 
-// Types of tiles in this map
-public enum Fortress_Tile
-{
-    Empty, // Nothing here, not yet used
-    Filled, // Something here, not yet used
-    Unused, // This tile will not be used
-    Tower, // Guard tower
-    Fountain, // A dried out fountain
-    Rubbish, // Small 1x1 building or an obstackle
-    Wall, // Procedurally generated outer wall
-    Building // Procedurally generated building
-}
-
 // Generating a map from a QR code
 public class Fortress_Generator : MonoBehaviour
 {
@@ -22,12 +9,25 @@ public class Fortress_Generator : MonoBehaviour
     //######################################  COMPONENTS  ######################################
     //##########################################################################################
 
+    // Types of tiles in this map
+    private enum Tile
+    {
+        Empty, // Nothing here, not yet used
+        Filled, // Something here, not yet used
+        Unused, // This tile will not be used
+        Tower, // Guard tower
+        Fountain, // A dried out fountain
+        Rubbish, // Small 1x1 building or an obstackle
+        Wall, // Procedurally generated outer wall
+        Building // Procedurally generated building
+    }
+
 
     //##########################################################################################
     //#############################  PRIVATE METHODS / VARIABLES  ##############################
     //##########################################################################################
 
-    private Fortress_Tile[,] Tiles = null; // 2D array of tiles that will be used for map generation
+    private Tile[,] Tiles = null; // 2D array of tiles that will be used for map generation
     private void Awake() => Generate_Map(); // Loading initial tiles and performing operations on them
 
 
@@ -39,8 +39,8 @@ public class Fortress_Generator : MonoBehaviour
     private void Generate_Map()
     {
         Load_Tiles();
+        Replace_Patterns();
         Log_Tiles();
-
         Add_Outer_Walls();
         Log_Tiles();
     }
@@ -89,16 +89,16 @@ public class Fortress_Generator : MonoBehaviour
             {1,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,0,1,0,0,1,0,1,0,0},
             {1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1}
         };
-        Tiles = (Fortress_Tile[,])(object)Tile_Indexes;
+        Tiles = (Tile[,])(object)Tile_Indexes;
     }
 
     // Adding an outer layer to the tileset
-    private void Add_Outer_Layer(Fortress_Tile tile)
+    private void Add_Outer_Layer(Tile tile)
     {
         // Creating new array of tiles
         int old_size = Tiles.GetLength(0);
         int new_size = old_size + 2;
-        Fortress_Tile [,] new_tiles = new Fortress_Tile[new_size, new_size];
+        Tile [,] new_tiles = new Tile[new_size, new_size];
         // Filling the new array of tiles
         for(int i = 0; i < new_size; i++)
         {
@@ -119,18 +119,66 @@ public class Fortress_Generator : MonoBehaviour
     // Adding outer walls and a spacing before it
     private void Add_Outer_Walls()
     {
-        if(Tiles[0, 0] != Fortress_Tile.Wall)
+        if(Tiles[0, 0] != Tile.Wall)
         {
-            if(Tiles[0, 0] != Fortress_Tile.Empty)
-                Add_Outer_Layer(Fortress_Tile.Unused);
-            Add_Outer_Layer(Fortress_Tile.Wall);
+            if(Tiles[0, 0] != Tile.Empty)
+                Add_Outer_Layer(Tile.Unused);
+            Add_Outer_Layer(Tile.Wall);
         }
     }
 
-    // Looking for a pattern and replacing it with a new one
-    private void Replace_Tiles(Fortress_Tile[,] pattern, Fortress_Tile[,] replacement)
+    // Attempting to replace a pattern with another one on a specific spot
+    private void Replace_Tile(Tile?[,] pattern, Tile?[,] replacement, int i, int j)
     {
+        // Checking if the patterns exists
+        for(int k = 0; k < pattern.GetLength(0); k++)
+            for(int l = 0; l < pattern.GetLength(1); l++)
+                if(pattern[k, l] != null && Tiles[i + k, j + l] != pattern[k, l])
+                    return;
+        // Overwriting the occurence of the pattern
+        for(int k = 0; k < pattern.GetLength(0); k++)
+            for(int l = 0; l < pattern.GetLength(1); l++)
+                if(replacement[k, l] != null)
+                    Tiles[i + k, j + l] = (Tile)replacement[k, l];
+    }
 
+    // Looking for a pattern and replacing it with a new one
+    private void Replace_All_Tiles(Tile?[,] pattern, Tile?[,] replacement)
+    {
+        if(pattern.GetLength(0) != replacement.GetLength(0) || pattern.GetLength(1) != replacement.GetLength(1))
+        {
+            Console.Warning(this, "Pattern and replacement sizes do not match!");
+            return;
+        }
+        int size = Tiles.GetLength(0);
+        for(int i = 0; i < size - pattern.GetLength(0) + 1; i++)
+            for(int j = 0; j < size - pattern.GetLength(1) + 1; j++)
+                Replace_Tile(pattern, replacement, i, j);
+    }
+
+    // Listing all patterns and their replacement
+    private void Replace_Patterns()
+    {
+        // Replacing towers
+        Tile? [,] tower_pattern = {
+            {Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled},
+            {Tile.Filled,Tile.Empty,Tile.Empty,Tile.Empty,Tile.Empty,Tile.Empty,Tile.Filled},
+            {Tile.Filled,Tile.Empty,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Empty,Tile.Filled},
+            {Tile.Filled,Tile.Empty,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Empty,Tile.Filled},
+            {Tile.Filled,Tile.Empty,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Empty,Tile.Filled},
+            {Tile.Filled,Tile.Empty,Tile.Empty,Tile.Empty,Tile.Empty,Tile.Empty,Tile.Filled},
+            {Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled,Tile.Filled}
+        };
+        Tile? [,] tower_replacement = {
+            {Tile.Tower,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused},
+            {Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused},
+            {Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused},
+            {Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused},
+            {Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused},
+            {Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused},
+            {Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused,Tile.Unused}
+        };
+        Replace_All_Tiles(tower_pattern, tower_replacement);
     }
 
 }
