93c8a0b41c8c3a46bb4b89b0d38db26a63175d9f
Václav Pilát <pilatvac@fit.cvut.cz>
2023-01-17 14:07:34 +0100

    Minor code update to Fortress generator script

--- Assets/Scripts/Maps/Fortress/Fortress_Generator.cs
+++ Assets/Scripts/Maps/Fortress/Fortress_Generator.cs
@@ -27,10 +27,8 @@ public class Fortress_Generator : MonoBehaviour
     //#############################  PRIVATE METHODS / VARIABLES  ##############################
     //##########################################################################################
 
-    private Fortress_Tile[,] Tiles = null;
-
-    // Loading initial tiles and performing operations on them
-    private void Awake() => Generate_Map();
+    private Fortress_Tile[,] Tiles = null; // 2D array of tiles that will be used for map generation
+    private void Awake() => Generate_Map(); // Loading initial tiles and performing operations on them
 
 
     //##########################################################################################
@@ -42,6 +40,7 @@ public class Fortress_Generator : MonoBehaviour
     {
         Load_Tiles();
         Log_Tiles();
+
         Add_Outer_Walls();
         Log_Tiles();
     }
@@ -49,7 +48,7 @@ public class Fortress_Generator : MonoBehaviour
     // Printing out tiles into console
     private void Log_Tiles()
     {
-        string output = "";
+        string output = "\n";
         int size = Tiles.GetLength(0);
         for(int i = 0; i < size; i++)
         {
@@ -93,36 +92,45 @@ public class Fortress_Generator : MonoBehaviour
         Tiles = (Fortress_Tile[,])(object)Tile_Indexes;
     }
 
-    // Adding outer walls and a spacing before it
-    private void Add_Outer_Walls()
+    // Adding an outer layer to the tileset
+    private void Add_Outer_Layer(Fortress_Tile tile)
     {
         // Creating new array of tiles
         int old_size = Tiles.GetLength(0);
-        int new_size = old_size + 4;
+        int new_size = old_size + 2;
         Fortress_Tile [,] new_tiles = new Fortress_Tile[new_size, new_size];
         // Filling the new array of tiles
         for(int i = 0; i < new_size; i++)
         {
             // Adding outer walls
-            new_tiles[0, i] = Fortress_Tile.Wall;
-            new_tiles[i, 0] = Fortress_Tile.Wall;
-            new_tiles[new_size - 1, i] = Fortress_Tile.Wall;
-            new_tiles[i, new_size - 1] = Fortress_Tile.Wall;
-            // Adding spacing before the wall
-            if(i >= 1 && i <= new_size - 2)
-            {
-                new_tiles[1, i] = Fortress_Tile.Unused;
-                new_tiles[i, 1] = Fortress_Tile.Unused;
-                new_tiles[new_size - 2, i] = Fortress_Tile.Unused;
-                new_tiles[i, new_size - 2] = Fortress_Tile.Unused;
-            }
+            new_tiles[0, i] = tile;
+            new_tiles[i, 0] = tile;
+            new_tiles[new_size - 1, i] = tile;
+            new_tiles[i, new_size - 1] = tile;
         }
         // Adding the original tiles
         for(int i = 0; i < old_size; i++)
             for(int j = 0; j < old_size; j++)
-                new_tiles[i + 2, j + 2] = Tiles[i, j];
+                new_tiles[i + 1, j + 1] = Tiles[i, j];
         // Setting the new tiles as the current ones
         Tiles = new_tiles;
+    }
+
+    // Adding outer walls and a spacing before it
+    private void Add_Outer_Walls()
+    {
+        if(Tiles[0, 0] != Fortress_Tile.Wall)
+        {
+            if(Tiles[0, 0] != Fortress_Tile.Empty)
+                Add_Outer_Layer(Fortress_Tile.Unused);
+            Add_Outer_Layer(Fortress_Tile.Wall);
+        }
+    }
+
+    // Looking for a pattern and replacing it with a new one
+    private void Replace_Tiles(Fortress_Tile[,] pattern, Fortress_Tile[,] replacement)
+    {
+
     }
 
 }
